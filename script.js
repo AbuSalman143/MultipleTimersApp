26c2df83ee50cb94053c2dc3dbe4ea917b853d48
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-01-13 20:15:15 +0530

    commit

--- script.js
+++ script.js
@@ -1,74 +1,110 @@
-    const setButton = document.getElementById("set-btn");
-    const activeTimersContainer = document.getElementById(
-    "active-timers-container"
-    );
-    const noTimerDisplayMessage = document.getElementById("no-timers-display-msg");
-    const audio = document.getElementById("audio-file");
-    let numberOfActiveTimers = 0;
+let hours=document.getElementById("hh");
+let minuts=document.getElementById("mm");
+let sec=document.getElementById("ss");
+let form=document.querySelector(".form");
+let timerlist=document.querySelector(".timerlist");
+let p=document.querySelector(".text");
+let audio=document.querySelector("audio");
+let numberOfActiveTimers = 0;
 
-    
-    function addNewTimer() {
-        const hours = Number(document.getElementById("hour").value);
-        const minutes = Number(document.getElementById("minute").value);
-        const seconds = Number(document.getElementById("seconds").value);
-    
+
+let total_time= 0;
+function updatetime(h,m,s){
+     total_time= h * 3600 + m * 60 + s;
+    if(total_time>0){
+        create_Element(h,m,s);
         
-        const totalTime = hours * 3600 + minutes * 60 + seconds;
-    
-        if (totalTime > 0) {
-        const newTimerCard = document.createElement("div");
-        newTimerCard.classList.add("timer-card");
-        newTimerCard.innerHTML = `
-            <span>Time Left :</span>
-            <span id="timer-container">
-                <input id="hour" type="number" min="0" max="24" placeholder="${hours}">
-                <input id="minute" type="number" min="0" max="60" placeholder="${minutes}">
-                <input id="seconds" type="number" min="0" max="60" placeholder="${seconds}">
-            </span>
-            <button class="btn" onclick="deleteTimer(this)">Delete</button>
-            `;
-        activeTimersContainer.appendChild(newTimerCard);
-        numberOfActiveTimers++;
-        noTimerDisplayMessage.style.display = "none";
-        runtimer(totalTime, newTimerCard);
-        }
     }
+}
+
+
+
+
+
+
+function create_Element(hh,mm,ss){
+
+    p.style.display="none";
+    let display=document.createElement("div");
+    display.classList.add("output-container");
+
+
+    // <div class="output-container">
+    //         <div class="lable">Time Left :</div>
+    //         <div class="op-time">
+    //             <div class="op-hh">hh</div>
+    //             <span>:</span>
+    //             <div class="op-mm">mm</div>
+    //             <span>:</span>
+    //            <div class="op-ss">ss</div>
+    //         </div>
+    //         <div class="delet"><button>Delete</button></div>
+    //     </div>
     
-    function runtimer(totalTime, newTimerCard) {
-        let timerContainer = newTimerCard.querySelector("#timer-container");
-        let hour = timerContainer.querySelector("#hour");
-        let minute = timerContainer.querySelector("#minute");
-        let seconds = timerContainer.querySelector("#seconds");
+
+    display.innerHTML=`<div class="lable">Time Left :</div>
+                         <div class="op-time">
+                            <div class="op-hh">${hh}</div>
+                            <span>:</span>
+                            <div class="op-mm">${mm}</div>
+                            <span>:</span>
+                            <div class="op-ss">${ss}</div>
+                        </div>
+                        <div class="delet"><button onclick="deleteTimer(this)">Delete</button></div>`
+
+    timerlist.appendChild(display); 
+    numberOfActiveTimers++; 
+    runtimer(total_time,display)
     
-        
-        const myTimerUpdate = setInterval(() => {
-        if (totalTime == 0) {
-            newTimerCard.classList.toggle("time-up");
-            newTimerCard.innerHTML = `
-                        <span></span>
-                        <span>Timer is Up!</span>
-                        <button class="btn delete-btn" onclick="deleteTimer(this)">Stop</button>
-                    `;
-            audio.play();
-            clearInterval(myTimerUpdate);
-        } else {
-            --totalTime;
-            hour.value = Math.floor(totalTime / 3600);
-            minute.value = Math.floor((totalTime % 3600) / 60);
-            seconds.value = totalTime % 60;
+}
+
+
+function runtimer(total_time,display){
+
+    let timerContainer = display.querySelector(".op-time");
+    let hour = timerContainer.querySelector(".op-hh");
+    let minute = timerContainer.querySelector(".op-mm");
+    let seconds = timerContainer.querySelector(".op-ss");
+
+    let myTimerupdate=setInterval(()=>{
+
+
+        if(total_time==0){
+            display.classList.toggle("time-up");
+            display.innerHTML=` <span></span>
+                                <span>Timer is Up!</span>
+                                <button class="btn delete-btn" onclick="deleteTimer(this)">Stop</button>`
+        audio.play();
+        clearInterval(myTimerupdate);                        
+        }else{
+
+
+            --total_time;
+            hour.innerText=Math.floor(total_time/3600);
+            minute.innerText=Math.floor((total_time%3600)/60);
+            seconds.innerText=Math.floor(total_time%60);
+            console.log(hour,minute,seconds);
         }
-        }, 1000);
-    }
+    },1000)
     
-    // delete timer function
-    function deleteTimer(deleteButton) {
-        audio.pause();
-        let currentTimerCard = deleteButton.parentNode;
-        currentTimerCard.remove();
-        --numberOfActiveTimers;
-        if (numberOfActiveTimers == 0) {
-        noTimerDisplayMessage.style.display = "block";
-        }
+}
+
+function deleteTimer(button){
+    audio.pause();
+    let current_display=button.parentNode;
+    current_display.remove();
+    --numberOfActiveTimers;
+    if(numberOfActiveTimers==0){
+        p.style.display="block";
     }
-    // event listeners
-    setButton.addEventListener("click", addNewTimer);
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let h=parseInt(hours.value)
+    let m=parseInt(minuts.value)
+    let s=parseInt(sec.value)
+    form.reset();
+    updatetime(h,m,s);
+    
+})
